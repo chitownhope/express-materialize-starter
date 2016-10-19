4ec99d47360643d2b9f17a45a68e96c420e6796b
chitownhope <apr_dec@hotmail.com>
2016-10-19 08:45:03 -0500

    breadcrumbs added

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,21 @@ let express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Tinas' })
 })
+
+router.get('/showcase', function(req, res, next){
+	res.render('showcase', {})
+})
+
+router.get('/breadcrumbs', function(req, res, next){
+	var crumbs = {
+		crumbs: ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth']
+	};
+	res.render('breadcrumbs', crumbs);
+		
+	});
+
+
 
 module.exports = router
